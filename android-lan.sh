29a521bd4c459ad4692da1352783f63e59de36b0
Archar Gelod <60652075+Archargelod@users.noreply.github.com>
2022-05-23 20:52:53 +0800

    clean up code and error handling

--- android-lan.sh
+++ android-lan.sh
@@ -10,9 +10,9 @@ case $@ in
                 exit 0
                 ;;
 	help|-h|--help)echo "usage: 
-		build/reinstall lan-play:    ./switchlan stable OR ./switchlan latest
-		connect to servers:          ./switchlan server:port
-                uninstall & delete lan-play: ./switchlan remove"
+		build/update lan-play:       ./switchlan stable OR ./switchlan latest
+		connect to servers:          ./switchlan <server's ip>:<port>
+                delete lan-play:             ./switchlan remove"
 		exit 0
 		;;
 esac
@@ -28,20 +28,17 @@ if [[ -f ~/switch-lan-play/build/src/lan-play ]] && [[ -z $version ]]; then
 fi
 
 [[ -z $version ]] && clear && echo -e "1 - stable / стабильная\n2 - latest / свежая\n" && read -n1 -p"Enter number / Введите цифру:" version && clear
+[[ $version != '1' ]] && [[ $version != '2' ]] && echo "Error: wrong version / неправильная версия" && exit 1
+
 
 apt update
 apt upgrade -y
-apt install libpcap -y
-apt install libuv -y
-pkg install cmake -y
-pkg install binutils -y
-pkg install tsu -y
-pkg install git -y
+apt install build-essential cmake binutils tsu git libpcap libuv -y
 
 case $version in
 	1)stable_tag=$(curl -s https://api.github.com/repos/spacemeowx2/switch-lan-play/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
 	  git clone --depth 1 --branch $stable_tag https://github.com/spacemeowx2/switch-lan-play.git
-		;;
+        ;;
 	2)if [[ -d ~/switch-lan-play ]]; then
               cd ~/switch-lan-play
               git pull https://github.com/spacemeowx2/switch-lan-play.git
@@ -49,13 +46,15 @@ case $version in
           else
               git clone https://github.com/spacemeowx2/switch-lan-play.git
           fi
-		;;
+	;;
 	*)echo "Error: wrong version"
 	  exit 1
-		;;
+	;;
 esac
 
 cd switch-lan-play || (echo 'Error cloning repo!' && exit 1)
 rm -rf ./build; mkdir ./build; cd ./build
-cmake -DCMAKE_BUILD_TYPE=Debug ..
-make
+cmake -DCMAKE_BUILD_TYPE=Debug .. || (echo "Error configuring lan-play" && exit 1)
+make || (echo "Error building lan-play" && exit 1)
+clear
+echo -e "\nDone!/Готово!\nNow you can launch app with './switchlan'\nТеперь можно запустить программу: './switchlan'"
